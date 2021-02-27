8698c0c506eff44d31f54fa98a5c398627a4e2f6
cahe6160 <cahe6160@colorado.edu>
2021-02-26 21:10:45 -0700

    grid controller added

---
+++ Flixster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flixster
+//
+//  Created by Caroline Hernandez on 2/26/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
--- Flixster/MoviesViewController.swift
+++ Flixster/MoviesViewController.swift
@@ -35,13 +35,10 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
             // access key in dictionary and cast
             self.movies = dataDictionary["results"] as! [[String:Any]]
             
+            // TODO: Reload your table view data
             self.tableView.reloadData()
             
             print(dataDictionary)
-            
-              // TODO: Get the array of movies
-              // TODO: Store the movies in a property to use elsewhere
-              // TODO: Reload your table view data
 
            }
         }
@@ -76,14 +73,28 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         
     }
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        print("loading up the details screen")
+        // sender = cell that was tapped on
+        // Find the selected movie
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)!
+        let movie = movies[indexPath.row]
+        
+        // Pass the selected movie to the movies details veiw controller
+        let detailsViewController = segue.destination as! MovieDetailsViewController
+        detailsViewController.movie = movie
+        
+        // so cell doesn't stay highlighted after selecting
+        tableView.deselectRow(at: indexPath, animated: true)
+        
     }
-    */
+    
 
 }
